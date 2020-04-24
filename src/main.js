902661526250b70a74c81651548e90911f66397e
Imre Mehesz <imehesz@gmail.com>
2020-04-24 11:18:36 -0400

    Cleanup, added short unformatted post support (entries??)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import * as riot from 'riot'
 import App from './app.riot'
 // import Woof from "./woof.riot"
-import BlogEntries from "./blog-entries.riot"
+import BlogPosts from "./blog-posts.riot"
+import BlogPostShortUnformatted from "./blog-post-short-unformatted.riot"
 
 
 // riot.register("woof", Woof)
-riot.register("blog-entries", BlogEntries)
+riot.register("blog-posts", BlogPosts)
+riot.register("blog-post-short-unformatted", BlogPostShortUnformatted)
 
 const mountApp = riot.component(App)
 
